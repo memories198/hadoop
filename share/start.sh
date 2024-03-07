0210d12d66e776f53c9ccb53a55ccf4cdad36b07
memories <1982002904@qq.com>
2024-03-07 21:20:51 +0800

    done

--- share/start.sh
+++ share/start.sh
@@ -1,3 +1,8 @@
+hadoop1RootPassword hadoop
+hadoop2RootPassword hadoop
+hadoop3RootPassword hadoop
+udbs01RootPassword oracle
+
 hadoop1:
   /app/hadoop/hadoop-2.6.4/sbin/start-dfs.sh
 
@@ -18,6 +23,16 @@ hadoop3:
 hadoop1:
    /app/hadoop/hbase-1.2.1/bin/start-hbase.sh
 
-
-
 -----
+hadoop1:
+  /opt/ggs/ggsci
+    start rephb
+    start mgr
+udbs01:
+  /u01/gg/11.2/ggsci
+    start pmp01
+    start ext01
+    start mgr
+
+
+
